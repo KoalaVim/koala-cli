2dd695ba193daf6f0f5fa3d8c5d30f25815e4e6c
Ofir Gal <ofir@gal.software>
2023-12-29 13:59:10 +0200

    added installers

---
+++ koala_cli/install/installers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from pathlib import Path
+
+
+def install_neovim(file: Path):
+    print(file)
+
+
+def install_nerdfont(file: Path):
+    print(file)
+
+
+def get_nerdfont() -> str:
+    return "CascadiaCode.tar.xz"
